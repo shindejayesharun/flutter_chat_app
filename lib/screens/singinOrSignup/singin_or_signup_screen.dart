82a2dce4ad3e2cda8d61bedd507ef1b35b835eb8
Shinde Jayesh Arun <shindejayesharun@gmail.com>
2021-06-01 22:24:39 +0530

    welcome screen added

---
+++ lib/screens/singinOrSignup/singin_or_signup_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignInOrSignUpScreen extends StatefulWidget {
+  @override
+  _SignInOrSignUpScreenState createState() => _SignInOrSignUpScreenState();
+}
+
+class _SignInOrSignUpScreenState extends State<SignInOrSignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/welcome/welcome.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/screens/singinOrSignup/singin_or_signup_screen.dart';
+
+import '../../constants.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Spacer(
+              flex: 2,
+            ),
+            Image.asset("assets/images/welcome_image.png"),
+            Spacer(
+              flex: 3,
+            ),
+            Text(
+              "Welcome to our freedom \nmessaging app",
+              textAlign: TextAlign.center,
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            Spacer(),
+            Text(
+              "Freedom talk any person of you \nmother language",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyText1.copyWith(
+                  color: Theme.of(context)
+                      .textTheme
+                      .bodyText1
+                      .color
+                      .withOpacity(0.8)),
+            ),
+            Spacer(
+              flex: 3,
+            ),
+            FittedBox(
+              child: TextButton(
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SignInOrSignUpScreen())),
+                child: Row(
+                  children: [
+                    Text(
+                      "Skip",
+                      style: Theme.of(context).textTheme.bodyText1.copyWith(
+                          color: Theme.of(context)
+                              .textTheme
+                              .bodyText1
+                              .color
+                              .withOpacity(0.8)),
+                    ),
+                    SizedBox(width: kDefaultPadding / 4),
+                    Icon(Icons.arrow_forward_ios,
+                    size: 16,color: Theme.of(context).textTheme.bodyText1.color.withOpacity(0.8),)
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
