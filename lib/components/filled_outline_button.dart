ef2016f857fd20757b1e886861ca0701b056a600
Shinde Jayesh Arun <shindejayesharun@gmail.com>
2021-06-01 22:51:30 +0530

    signin screen added

---
+++ lib/components/filled_outline_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/components/primary_button.dart';
+import 'package:flutter_chat_app/constants.dart';
+
+class FilledOutlineButton extends StatelessWidget {
+  final bool isFilled;
+  final VoidCallback press;
+  final String text;
+
+  const FilledOutlineButton({Key key, this.isFilled, this.press, this.text}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+        side: BorderSide(color: Colors.white)
+      ),
+      elevation: isFilled ? 2 : 0,
+      color: isFilled ? Colors.white : Colors.transparent,
+      onPressed: press,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: isFilled ? kContentColorLightTheme : Colors.white,
+          fontSize: 12
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/singinOrSignup/singin_or_signup_screen.dart
+++ lib/screens/singinOrSignup/singin_or_signup_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/components/primary_button.dart';
+import 'package:flutter_chat_app/constants.dart';
+import 'package:flutter_chat_app/screens/chats/chat_screen.dart';
 
 class SignInOrSignUpScreen extends StatefulWidget {
   @override
@@ -8,6 +11,36 @@ class SignInOrSignUpScreen extends StatefulWidget {
 class _SignInOrSignUpScreenState extends State<SignInOrSignUpScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Column(
+            children: [
+              Spacer(
+                flex: 2,
+              ),
+              Image.asset(
+                  MediaQuery.of(context).platformBrightness == Brightness.light
+                      ? "assets/images/Logo_light.png"
+                      : "assets/images/Logo_dark.png"),
+              Spacer(),
+              PrimaryButton(
+                text: "Sign In",
+                press: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ChatScreen())),
+              ),
+              SizedBox(
+                height: kDefaultPadding * 1.5,
+              ),
+              PrimaryButton(
+                color: Theme.of(context).colorScheme.secondary
+              ,text: "Sign Up", press: () {}),
+              Spacer(flex: 2,),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
